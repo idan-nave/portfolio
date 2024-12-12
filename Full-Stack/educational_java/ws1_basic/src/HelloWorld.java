b60dec55147263589f242d5dffcba9d43e494094
idan <idan1980394@gmail.com>
2024-12-12 15:36:16 +0200

    gitignore cahnge

---
+++ Full-Stack/educational_java/ws1_basic/src/HelloWorld.java
@@ -0,0 +1,16 @@
+/********************************** ws_x.java  ******************************/
+/*
+  Name: Idan Nave
+  Project: Work Sheet #
+  Reviewer: ???
+  Date: 12.12.24
+  Version: 1.0
+*/
+/******************************** Libraries  **********************************/
+
+
+public class HelloWorld {
+  public static void main(String[] args) {
+  System.out.println("Hello, World!");
+  }
+  }
---
+++ Full-Stack/educational_java/ws1_basic/test/Two.java
@@ -0,0 +1,16 @@
+/********************************** Test_ws_x.java  ******************************/
+/*
+  Name: Idan Nave
+  Project: Work Sheet #
+  Reviewer: ???
+  Date: 12.12.24
+  Version: 1.0
+*/
+
+package test;
+
+public class Two {
+  public static void main(String[] args) {
+    System.out.println("Two");
+  }
+}
